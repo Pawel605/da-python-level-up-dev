d70d13e6c003447541d15b7bd6f0042d101e75bd
Paweł F <pawel605@gmail.com>
2022-05-16 21:20:06 +0200

    Add solution for task 1.2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 from pydantic import BaseModel
 
@@ -27,4 +27,13 @@ async def read_item(name: str):
 def root():
     return {"start": "1970-01-01"}
 
+
+# task 1.2
+@app.get("/method", status_code=200)
+@app.post("/method", status_code=201)
+@app.delete("/method", status_code=200)
+@app.put("/method", status_code=200)
+@app.options("/method", status_code=200)
+def read_request(request: Request):
+    return {"method": request.method}
 
