db657a62a60c7d37e5394168d7208caa2c085a20
Paweł F <pawel605@gmail.com>
2022-05-16 21:56:30 +0200

    Add tests for task 1.1

---
+++ task_1_test_1.py
@@ -0,0 +1,25 @@
+import unittest
+
+from fastapi.testclient import TestClient
+
+from main import app
+
+client = TestClient(app)
+
+
+class HerokuSetupTest(unittest.TestCase):
+
+    def test_status_code(self):
+        response = client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_response(self):
+        response = client.get("/")
+        self.assertEqual(
+            response.json(),
+            {"start": "1970-01-01"},
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
