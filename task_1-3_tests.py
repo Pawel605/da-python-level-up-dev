9b33efc41810f331a804569d53a5ba418a971795
Paweł F <pawel605@gmail.com>
2022-05-16 22:12:39 +0200

    Add tests for task 1.3

--- task_1-3_tests.py
+++ task_1-3_tests.py
@@ -8,50 +8,30 @@ client = TestClient(app)
 
 
 class HerokuSetupTest(unittest.TestCase):
-    def test_get(self):
-        response = client.get("/method")
 
+    def test_monday_correct(self):
+        response = client.get("/day?name=monday&number=1")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(
-            response.json(),
-            {"method": "GET"},
-        )
 
-    def test_post(self):
-        response = client.post("/method")
-
-        self.assertEqual(response.status_code, 201)
-        self.assertEqual(
-            response.json(),
-            {"method": "POST"},
-        )
-
-    def test_put(self):
-        response = client.put("/method")
-
+    def test_tuesday_correct(self):
+        response = client.get("/day?name=tuesday&number=2")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(
-            response.json(),
-            {"method": "PUT"},
-        )
 
-    def test_options(self):
-        response = client.options("/method")
-
+    def test_saturday_correct(self):
+        response = client.get("/day?name=saturday&number=6")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(
-            response.json(),
-            {"method": "OPTIONS"},
-        )
 
-    def test_delete(self):
-        response = client.delete("/method")
-
+    def test_sunday_correct(self):
+        response = client.get("/day?name=sunday&number=7")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(
-            response.json(),
-            {"method": "DELETE"},
-        )
+
+    def test_nonday_404(self):
+        response = client.get("/day?name=nonday&number=2")
+        self.assertEqual(response.status_code, 400)
+
+    def test_sunday_4_404(self):
+        response = client.get("/day?name=sunday&number=4")
+        self.assertEqual(response.status_code, 400)
 
 
 if __name__ == "__main__":
