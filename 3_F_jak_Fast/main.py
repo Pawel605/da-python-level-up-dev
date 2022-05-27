9cb87254f56cbe3ab1255df191fda1b7394bf82e
Paweł F <pawel605@gmail.com>
2022-05-27 22:21:09 +0200

    Add solution for task 3.2

--- 3_F_jak_Fast/main.py
+++ 3_F_jak_Fast/main.py
@@ -1,5 +1,10 @@
-from fastapi import FastAPI
+import datetime
+
+from fastapi import FastAPI, Depends, security, status, HTTPException
 from fastapi.responses import HTMLResponse
+from fastapi.security import HTTPBasicCredentials, HTTPBasic
+from datetime import date
+from utils import calculate_age
 
 app = FastAPI()
 
@@ -8,3 +13,26 @@ app = FastAPI()
 @app.get("/start", response_class=HTMLResponse)
 def read_root_start():
     return """<h1>The unix epoch started at 1970-01-01</h1>"""
+
+
+# Task 3.2
+@app.post("/check", response_class=HTMLResponse)
+def user_authorization(credentials: HTTPBasicCredentials = Depends(HTTPBasic())):
+    try:
+        datetime.datetime.strptime(credentials.password, "%Y-%m-%d")
+    except ValueError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid password, correct date format is YYYY-MM-DD",
+        )
+
+    date_of_birth = date.fromisoformat(credentials.password)
+    user_age = calculate_age(date_of_birth)
+
+    if user_age < 16:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="You are under 16 years of age!",
+        )
+    else:
+        return f"""<h1>Welcome {credentials.username}! You are {user_age}</h1>"""
---
+++ 3_F_jak_Fast/utils.py
@@ -0,0 +1,11 @@
+import datetime
+from datetime import date
+
+
+def calculate_age(birthdate: datetime):
+    today = date.today()
+    return (
+        today.year
+        - birthdate.year
+        - ((today.month, today.day) < (birthdate.month, birthdate.day))
+    )
